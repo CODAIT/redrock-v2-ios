d2295853ae38811035e82b2afc594c60dca40de0
Jonathan Alter <jonalter@users.noreply.github.com>
2016-06-02 14:04:10 -0700

    Adding padding at the beginning of the search field

---
+++ RedRock/SearchField.swift
@@ -0,0 +1,43 @@
+//
+//  SearchField.swift
+//  RedRock3
+//
+//  Created by Jonathan Alter on 6/2/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+/**
+ * (C) Copyright IBM Corp. 2015, 2015
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import UIKit
+
+class SearchField: UITextField {
+    
+    let padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20);
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+}
